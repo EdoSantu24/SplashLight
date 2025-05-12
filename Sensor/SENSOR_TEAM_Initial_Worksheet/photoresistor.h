c9ccf34a5c3521e2941482d60d6d03abb25f098f
ZaakoArolikin <61696933+ZaakoArolikin@users.noreply.github.com>
2025-05-12 12:20:42 +0200

    Update photoresistor.h

--- Sensor/SENSOR_TEAM_Initial_Worksheet/photoresistor.h
+++ Sensor/SENSOR_TEAM_Initial_Worksheet/photoresistor.h
@@ -4,7 +4,7 @@
 #define PHOTORESISTOR_H
 
 #define PHOTO_RES_1 0
-#define TURN_ON_PHOTORESISTOR_PIN 26
+//#define TURN_ON_PHOTORESISTOR_PIN 26
 
 void setupPhotoresistor();
 int readPhotoresistor();
